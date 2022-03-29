2411b2b99809f0fc82159ed93b9994c5b26c6b78
annicaholst <90824518+annicaholst@users.noreply.github.com>
2022-03-29 12:25:56 +0200

    update

---
+++ js/js.js
@@ -0,0 +1,23 @@
+
+
+/** Baggrund sky **/
+
+let rellax = new Rellax ('.rellax')
+
+
+// billeder med audio
+
+let t= document.getElementById('test');
+
+function minFunktion(){
+  besked.innerHTML = `
+  <nav>
+      <ul>
+      <p>Patrcia har et secret crush på en i klassen</p>
+    </ul>
+  </nav>
+  `;
+  test.style.display = 'none';
+}
+
+t.addEventListener('click', minFunktion);
